99b4623ee801909cc1a2e2b050365c2b7157669c
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-02-28 10:52:11 -0600

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 mongoose.set('useCreateIndex', true)
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/group-5-project-3", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/group-5-project-3",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
