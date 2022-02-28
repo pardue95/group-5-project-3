a6331e057c2430a970068f1f7a5d4066318b1b03
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-02-28 11:07:20 -0600

    heroku

--- server/server.js
+++ server/server.js
@@ -30,9 +30,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+
 
 db.once("open", () => {
   app.listen(PORT, () => {
